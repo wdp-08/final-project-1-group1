63a53868e82994a2cce98626e96b154814016540
SalmO-On <fitrianurul0213@gmail.com>
2023-04-25 21:55:53 +0700

    memperbarui tampilan login dan signup

---
+++ js/login.js
@@ -0,0 +1,19 @@
+
+var form = document.querySelector('form');
+var usernameInput = document.getElementById('username');
+var passwordInput = document.getElementById('password');
+var status = document.getElementById('status');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); // mencegah form untuk mengirimkan data dan melakukan refresh halaman
+    var username = usernameInput.value;
+    var password = passwordInput.value;
+    var storedUsername = localStorage.getItem('username');
+    var storedPassword = localStorage.getItem('password');
+
+    if (username === storedUsername && password === storedPassword) {
+        window.location.href = 'index.html'; // mengarahkan ke halaman home jika login berhasil
+    } else {
+        status.textContent = 'Invalid username or password';
+    }
+});
